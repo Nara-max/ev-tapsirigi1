7097b6e3682564cb34575ec92e69e74745f17c49
Nara-max <hamzayevanargiz99@gmail.com>
2024-09-14 07:56:54 +0100

    frist task

---
+++ Ev tapsirigi/Program.cs
@@ -0,0 +1,29 @@
+﻿
+//Console.WriteLine("Hello, World!");
+
+//int a = 3;
+//int b = 5;
+
+//Console.WriteLine(a+(b/2));
+
+
+string mom = "anne";
+string dad="baba";
+string meslek = "doktor";
+Console.WriteLine($"{mom} {dad} {meslek}");
+
+char basherf = 'N';
+Console.WriteLine(basherf);
+
+float c = 3.5F;
+float d = 4.5F;
+Console.WriteLine(d - c);
+
+double pizza = 10.99D;
+double service_charge = 2.99D;
+
+Console.WriteLine(pizza-service_charge);
+
+decimal ilkfiyat= 20M;
+decimal indirim= 10M;
+Console.WriteLine(ilkfiyat - indirim);
